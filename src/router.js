943bcc7a4d62c10cc254df00e0074cf492805e7c
luiss1569 <rcsrcs1569@gmail.com>
2020-05-25 00:23:21 -0300

    comunicação basica concluida

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
-import App from './pages/App'
+import GameM from './pages/GameM'
+import GameS from './pages/GameS'
 
 export default function Routes(){
     return(
         <BrowserRouter>
-            <Route path="/" exact component={App}/>
+            <Route path="/" exact component={GameM}/> 
+            <Route path="/:gameid/game" exact component={GameS}/>
         </BrowserRouter>
     )
 }
